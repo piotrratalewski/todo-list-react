bdc3ae85528b2c70ac7c41a434896cea1c7ed4b3
Piotr Ratalewski <piotr.ratalewski@gmail.com>
2023-07-06 19:23:14 +0200

    some changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<body className='body'>
+			<main className='container'>
+				<header>
+					<h1>Lista zadań</h1>
+				</header>
+				<section className='section'>
+					<h2 className='section__title'>Dodaj nowe zadanie</h2>
+					<form className='form js-form'>
+						<input
+							required
+							className='form__input js-newTask'
+							placeholder='Co jest do zrobienia?'
+							autofocus
+						/>
+						<button className='form__button'>Dodaj zadanie</button>
+					</form>
+				</section>
+				<section className='section'>
+					<header className='section__header'>
+						<h2 className='section__title'>Lista zadań</h2>
+						<div className='buttons js-buttons'>
+							<button className='buttons_button js-toggleHideDoneTasks'>
+								Ukryj ukończone
+							</button>
+							<button className='buttons_button js-markAllDone'>
+								Ukończ wszystkie
+							</button>
+						</div>
+					</header>
+					<ul className='tasks js-tasks'></ul>
+				</section>
+			</main>
+		</body>
+	);
 }
 
 export default App;
